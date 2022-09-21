a125ae50f96e75122c8a324e32bc282ecef90b28
sloane <jemma.bee@gmail.com>
2022-09-22 01:35:24 +0300

    added

--- HW_3/Program.cs
+++ HW_3/Program.cs
@@ -28,7 +28,7 @@ else Console.WriteLine($"Input the right number");
 A (3,6,8); B (2,1,-7), -> 15.84
 A (7,-5, 0); B (1,-1,9) -> 11.53
 */
-/*Console.WriteLine("\n Задача 21");
+
 int x1 = Coordinate("x", "A");
 int y1 = Coordinate("y", "A");
 int z1 = Coordinate("z", "A");
@@ -38,51 +38,52 @@ int z2 = Coordinate("z", "B");
 
 int Coordinate(string coorName, string pointName)
 {
-    Console.Write($"Введите координату {coorName} точки {pointName}: ");
-    return Convert.ToInt16(Console.ReadLine());
+Console.Write($"Input coordinate {coorName} of  {pointName}: ");
+ return Convert.ToInt16(Console.ReadLine());
 }
 
 double Decision(double x1, double x2, 
-                double y1, double y2, 
-                double z1, double z2){
-  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
-                   Math.Pow((y2-y1), 2) + 
-                   Math.Pow((z2-z1), 2));
+double y1, double y2, 
+double z1, double z2){
+return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+Math.Pow((y2-y1), 2) + 
+Math.Pow((z2-z1), 2));
 }
 
 double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
 
-Console.WriteLine($"Длина отрезка  {segmentLength}");
-*/
+Console.WriteLine($"Run length  {segmentLength}");
+
 /*
 Задача 23
 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125
 */
-/* Console.WriteLine("\n Задача 23");
-Console.Write("Введите число: ");
+
+Console.Write("Input number: ");
 int cube = Convert.ToInt32(Console.ReadLine());
 
-void Cube(int[] cube){
-  int counter = 0;
-  int length = cube.Length;
-  while (counter <  length){
-    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
-    counter++;
-  }
+void Cube(int[] cube)
+{
+int counter = 0;
+int length = cube.Length;
+while (counter <  length)
+{
+cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+counter++;
 }
-
-void PrintArry(int[] coll){
-  int count = coll.Length;
-  int index = 0;
-  while(index < count){
-    Console.Write(coll[index]+ " ");
-    index++;
-  }
+}
+void PrintArry(int[] coll)
+{
+int count = coll.Length;
+int index = 0;
+while(index < count)
+{
+Console.Write(coll[index]+ " ");
+index++;
+}
 } 
-
 int[] arry = new int[cube+1];
 Cube(arry);
 PrintArry(arry);
-*/
