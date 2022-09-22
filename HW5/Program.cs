134c6792a5f6d4982b9c77b19113ea3aa159e6cc
sloane <jemma.bee@gmail.com>
2022-09-22 17:18:42 +0300

    added

--- HW5/Program.cs
+++ HW5/Program.cs
@@ -32,16 +32,88 @@ count++;
 Console.WriteLine($"There are {newArray.Length} numbers, {count} are even.");
 
 
-
-
-
-
-
 /*
 Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0
+*/
 
+void FillArrayRandomNumbers(int[] numbers)
+{
+for(int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(1,100);
+}
+}
+
+void ShowArray (int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+{
+    Console.Write (array [i] + ", ");
+}
+Console.Write("] ");
+}
+
+Console.WriteLine("Input size of array");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Masive is: ");
+ShowArray(numbers);
+int sum = 0;
+
+for (int k = 0; k < numbers.Length; k+=2)
+    sum = sum + numbers[k];
+Console.WriteLine($"There are {numbers.Length} numbers, sum of elements at odd positions = {sum}");
+
+
+
+/*
 Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 [3 7 22 2 78] -> 76
 */
+
+Console.WriteLine("Input size of array");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Massive is: ");
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int x = 0; x < numbers.Length; x++)
+{
+if (numbers[x] > max)
+{
+    max = numbers[x];
+}
+if (numbers[x] < min)
+{
+    min = numbers[x];
+}
+}
+
+Console.WriteLine($"There are {numbers.Length} numbers. Maximum is = {max}, minimum is = {min}");
+Console.WriteLine($"Interval between them = {max - min}");
+
+void FillArrayRandomNumbers(double[] numbers)
+{
+ for(int i = 0; i < numbers.Length; i++)
+    {
+    numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 1;
+    }
+}
+void PrintArray(double[] numbers)
+{
+Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+{
+Console.Write(numbers[i] + ", ");
+}
+Console.Write("]");
+Console.WriteLine();
+}
+
