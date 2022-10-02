6dd25f29d645fd47838b8fd041401fa3350d0eb5
sloane <jemma.bee@gmail.com>
2022-10-02 21:48:22 +0300

    readme added

--- Final/Program.cs
+++ Final/Program.cs
@@ -6,63 +6,27 @@
 		[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 		[“Russia”, “Denmark”, “Kazan”] → []
 */
-using System;
-					
-public class Program
+string[] array1 = new string[4] {"Hello", "2", "world", ":-)" };
+string[] array2 = new string[array1.Length];
+void SecondArray(string[] array1, string[] array2)
 {
-	public static void Main()
-	{
-		
-		string[] arr1 = {"Hello", "2", "world", ":-)"};
-		string[] arr2 = {"1234", "1567", "-2", "computer science"};
-		string[] arr3 = {"Russia", "Denmark", "Kazan"};
-		
-		int MAX_WORD_LENGTH = 3;
-		
-		string[] GetShortWordsArray(string[] array)
-		{
-			int length = array.Length;			
-			string[] result = new string[length];
-			int count = 0;
-			
-			for (int i = 0; i < length; i++)
-			{
-				if(array[i].Length <= MAX_WORD_LENGTH)
-				{
-					result[count] = array[i];
-					count++;
-                }
-            }
-        Array.Resize(ref result, count);
-			
-			return result;
-		}
-		
-		void PrintArray(string[] array)
-		{
-			if(array.Length == 0)
-			{
-				Console.WriteLine("Array is empty");
-			}
-			else
-			{
-				Array.ForEach(array, (str) => Console.Write($"{str} "));
-				Console.WriteLine();
-			}
-		}
-		
-		void PrintTask(string[] array)
-		{
-			Console.WriteLine("Source array:");
-			PrintArray(array);
-			string[] shortArray = GetShortWordsArray(array);
-			Console.WriteLine($"Result array (words are less or equal than {MAX_WORD_LENGTH} symbols)");
-			PrintArray(shortArray);
-			Console.WriteLine();
-		}
-		
-		PrintTask(arr1);
-		PrintTask(arr2);
-		PrintTask(arr3);
-	}
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
 }
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(array1, array2);
+PrintArray(array2);
