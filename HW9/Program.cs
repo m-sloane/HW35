236dbb70f2b21cf341e9282c920a48ac0d9bc70d
sloane <jemma.bee@gmail.com>
2022-10-02 20:53:13 +0300

    added

--- HW9/Program.cs
+++ HW9/Program.cs
@@ -56,29 +56,29 @@ int InputNumber(string input)
 Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 m = 2, n = 3 -> A(m,n) = 29
 */
+int m = InputNumber("Input m: ");
+int n = InputNumber("Input n: ");
+int akkermanFunction = Akkerman(m, n);
+Console.Write($"m = {m}, n = {n} - > A(m,n) = {akkermanFunction} ");
 int InputNumber(string input)
 {
-Console.Write(input);
-int output = Convert.ToInt32(Console.ReadLine());
-return output;
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
 }
 int Akkerman(int m, int n)
 {
 if (m == 0)
-{
-return n + 1;
-}
+    {
+    return n + 1;
+    }
 else if (n == 0 && m > 0)
-{
-return Akkerman(m - 1, 1);
-}
+    {
+    return Akkerman(m - 1, 1);
+    }
 else
-{
-return Akkerman(m - 1, Akkerman(m, n - 1));
-}
+    {
+    return Akkerman(m - 1, Akkerman(m, n - 1));
+    }
 }
 
-int m = InputNumber("Ввод m: ");
-int n = InputNumber("Ввод n: ");
-int akkermanFunction = Akkerman(m, n);
-Console.Write($"m = {m}, n = {n} - > A(m,n) = {akkermanFunction} ");
