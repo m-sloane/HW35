f020fc4c7e9b73cfb6a93075b81c31db5a8ca80c
sloane <jemma.bee@gmail.com>
2022-10-02 23:21:20 +0300

    added

--- HW9/Program.cs
+++ HW9/Program.cs
@@ -3,6 +3,7 @@ M = 1; N = 5. -> ""1, 2, 3, 4, 5""
 M = 4; N = 8. -> ""4, 6, 7, 8""
 */
 
+/*
 Console.WriteLine("Enter N: ");
 int numStart = 1;
 int numFinish = Convert.ToInt32(Console.ReadLine());
@@ -17,7 +18,7 @@ int NumbersToN(int number1, int number2)
     return NumbersToN(number1++, number2);
 }
 NumbersToN(numStart,numFinish);
-
+*/
 
 /*
 Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
@@ -52,10 +53,14 @@ int InputNumber(string input)
  Console.Write($"{m}, {n} ->");
  PrintSum(m, n, temp = 0); 
  
+
+
 /*
 Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 m = 2, n = 3 -> A(m,n) = 29
 */
+
+/*
 int m = InputNumber("Input m: ");
 int n = InputNumber("Input n: ");
 int akkermanFunction = Akkerman(m, n);
@@ -81,4 +86,4 @@ else
     return Akkerman(m - 1, Akkerman(m, n - 1));
     }
 }
-
+*/
